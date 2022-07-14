9c63620e8ea8aa5011127a5e0dd1bb9ea2fd74b8
dhirgaik <dhirgaik@LIN24010519.corp.capgemini.com>
2022-07-14 10:59:11 +0530

    Added First, Second and consumer

---
+++ Documents/workpsace-STS-MS1/first-service/src/main/java/com/cg/mc/controller/FirstServiceController.java
@@ -0,0 +1,16 @@
+package com.cg.mc.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstServiceController {
+
+	
+	@GetMapping("/first")
+	public String getFirstMessage(){
+		
+		return "Hello Mister";
+	}
+	
+}
